4f8560ec71ef60902fa342894ab85989ed5e9cb0
USER <bee31686@gmail.com>
2022-09-08 19:17:30 +0800

    Initial commit

---
+++ src/main/java/com/example/dameapp/DameappApplication.java
@@ -0,0 +1,13 @@
+package com.example.dameapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DameappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DameappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dameapp/DameappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dameapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DameappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
